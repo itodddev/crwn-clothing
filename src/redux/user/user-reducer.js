a5d8a06668c6d6411f17c7cf8414697523bd7c17
Todd James <todd@itodd.co>
2020-11-15 09:22:35 -0500

    updating our app to rediect to home if user is signed in

--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from './user.types';
+
 const INITIAL_STATE = {
   currentUser: null
 }
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 // state - default value = INITIAL_STATE, if state is undefind then default to INITIAL_STATE, if state is defined use current state
 const userReducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
-    case 'SET_CURRENT_USER':
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
